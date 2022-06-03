e7b0db1f9a3beb1850a31b28621cbe852b484502
matecmi <kevin2010_12@hotmail.com>
2022-06-03 18:47:17 -0500

    Nombre del Proyecto:my-app, Descripcion:version final del proyecto my-app

---
+++ src/main/java/com/diego/myapp/entity/TestEntity.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.diego.myapp.entity;
+
+/**
+ * @author kevin
+ *
+ */
+public class TestEntity {
+	
+	/**
+	 * propiedad de prueba para la muestra de compilación del archivo testentity
+	 */
+	private String nombre;
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
